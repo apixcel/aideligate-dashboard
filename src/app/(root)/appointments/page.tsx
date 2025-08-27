dd7fc70aabe02472155859d6aeeaaa77b15c8725
Nazmul Islam Sakib <sakibsarkar>
2025-08-27 15:55:05 +0600

    feat: update

--- src/app/(root)/appointments/page.tsx
+++ src/app/(root)/appointments/page.tsx
@@ -3,21 +3,24 @@ import { SectionSubTitle, SectionTitle } from "@/components";
 import AppointmentTable from "@/components/appointment/AppointmentTable";
 import CreateAppointment from "@/components/appointment/CreateAppointment";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 const Page = () => {
   const [refetch, setRefetch] = useState(0);
+  const { t } = useTranslation();
   return (
     <div className="flex w-full flex-col gap-6">
       <SectionTitle
-        title="Appointments"
+        title={t("dashboard_Overview.appointments")}
         description="Manage and track all your appointments. New appointments will appear automatically."
       />
       <div className="flex flex-col gap-6 rounded-xl border border-darker bg-darkest p-6 transition-all duration-300">
         <div className="flex items-center justify-between gap-[10px]">
           <SectionSubTitle title="All Appointments" description="View filter/create appointments" />
           <div className="flex items-center justify-between">
-            <CreateAppointment onSuccess={() => setRefetch(refetch + 1)}/>
+            <CreateAppointment onSuccess={() => setRefetch(refetch + 1)} />
           </div>
         </div>
+        {/* <p>{t("dashobarad")}</p> */}
         <AppointmentTable refetch={refetch} setRefetch={setRefetch} />
       </div>
     </div>
